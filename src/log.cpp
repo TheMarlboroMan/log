fa6445590268e4c909dd07ea4558ffe553470e92
TheMarlboroMan <marlborometal@gmail.com>
2018-07-14 13:37:16 +0200

    New doxygen docs

--- src/log.cpp
+++ src/log.cpp
@@ -2,17 +2,13 @@
 
 using namespace tools;
 
-//!Converts integer to lcut values.
+lcut	log::int_to_lcut(int v) {
 
-lcut	log::int_to_lcut(int v)
-{
-	if(v < 0 || v > 3)
-	{
+	if(v < 0 || v > 3) {
 		throw std::runtime_error("Invalid cut level "+std::to_string(v)+" specified for int_to_lcut");
 	}
 
-	switch(v)
-	{
+	switch(v) {
 		case error: 	return lcut::error; break;
 		case warning: 	return lcut::warning; break;
 		case info: return lcut::info; break;
@@ -25,50 +21,35 @@ lcut	log::int_to_lcut(int v)
 
 log::log()
 	:s(), entry_level(levels::all), min_level(levels::all), 
-	active(false)
-{
+	active(false) {
 
 }
 
 log::log(const char * filename)
 	:s(), entry_level(levels::all), min_level(levels::all), 
-	active(true)
-{
+	active(true) {
+
 	init(filename);
 }
 
-//!Class destructor.
+log::~log() {
 
-//!Adds a timestamp.
-
-log::~log()
-{
-	if(is_usable())
-	{
+	if(is_usable()) {
 		(*this)<<"Session ends "<<ltime::datetime<<std::endl;
 		s.close();
 	}
 }
 
-//!Activates the log.
+void log::activate() {
 
-//!Adds a timestamp.
-
-void log::activate() 
-{
 	active=true;
-	if(is_usable())
-	{
+	if(is_usable()) {
 		(*this)<<"Session starts "<<ltime::datetime<<std::endl;
 	}
 }
 
-//!Dectivates the log.
+void log::deactivate() {
 
-//!Adds a timestamp.
-
-void log::deactivate() 
-{
 	if(is_usable())
 	{
 		(*this)<<"Session ends "<<ltime::datetime<<std::endl;
@@ -77,12 +58,7 @@ void log::deactivate()
 	active=false;
 }
 
-//!Inits the log, opening the file.
-
-//!Adds a timestamp.
-
-void log::init(const char * filename)
-{
+void log::init(const char * filename) {
 	s.open(filename);
 
 	if(is_usable())
@@ -91,27 +67,16 @@ void log::init(const char * filename)
 	}
 }
 
-//!Locks or unlocks the log for multithreading environments.
+log& log::operator<<(lop op) {
 
-//!In multithreaded environments, the log must be locked before writing
-//!and unlocked when done.
-
-log& log::operator<<(lop op)
-{
-	switch(op)
-	{
+	switch(op) {
 		case lop::lock: 	mtx.lock(); break;
 		case lop::unlock: 	mtx.unlock(); break;
 	}
 	return *this;
 }
 
-//!Sets the level of input.
-
-//!Additionally adds the level to the output.
-
-log& log::operator<<(lin lvl)
-{
+log& log::operator<<(lin lvl) {
 	switch(lvl)
 	{
 		case lin::error:
@@ -130,10 +95,6 @@ log& log::operator<<(lin lvl)
 	return *this;
 }
 
-//!Sets the minimum level. 
-
-//!Error will show only errors, warning will show errors and warnings, etc.
-
 log& log::operator<<(lcut lvl)
 {
 	switch(lvl)
@@ -146,14 +107,10 @@ log& log::operator<<(lcut lvl)
 	return *this;
 }
 
-//!Adds the time, date or both to the output.
+log& log::operator<<(ltime v) {
 
-log& log::operator<<(ltime v)
-{
-	if(is_usable())
- 	{
-		switch(v)
-		{
+	if(is_usable()) {
+		switch(v) {
 			case ltime::time:
 				operator<<("[")<<time()<<"] ";
 			break;
@@ -169,10 +126,8 @@ log& log::operator<<(ltime v)
 	return *this;
 }
 
-//!Creates a string with the system time.
-
-std::string log::time() const
-{
+//TODO: There are C++ ways of doing this.
+std::string log::time() const {
 	char * t=new char[14];
 	memset(t, '\0', 14);
 	time_t tiempo=std::time(nullptr);
@@ -182,10 +137,8 @@ std::string log::time() const
 	return res;
 }
 
-//!Creates a string with the system date.
-
-std::string log::date() const
-{
+//TODO: There are C++ ways of doing this.
+std::string log::date() const {
 	char * t=new char[14];
 	memset(t, '\0', 14);
 	time_t tiempo=std::time(nullptr);
@@ -195,18 +148,13 @@ std::string log::date() const
 	return res;
 }
 
-//!Accepts std::ostream modificators.
-
 log& log::operator<<(std::ostream& ( *pf )(std::ostream&))
 {
-	if(is_usable() && check_levels())
-	{
+	if(is_usable() && check_levels()) {
 		s<<pf;
 	}
 	return *this;
 }
-
-//!Accepts std::ios modificators.
 
 log& log::operator<<(std::ios& ( *pf )(std::ios&))
 {
@@ -216,8 +164,6 @@ log& log::operator<<(std::ios& ( *pf )(std::ios&))
 	}
 	return *this;
 }
-
-//!Accepts std::ios_base modificators.
 
 log& log::operator<<(std::ios_base& ( *pf )(std::ios_base&))
 {
--- src/log.h
+++ src/log.h
@@ -25,28 +25,40 @@ enum class ltime{date, time, datetime};
 
 //!The log allows for four levels (all, info, warning, error) of input, but
 //!can be cut at any level to produce smaller files. It also has locking
-//!capabilities for multithreading and date/time formats.
+//!capabilities for multithreading and date/time formats. Logs might be 
+//!inactive, on which case operations can be called upon them, but no logging
+//!will happen.
 
 class log
 {
 	public:
 
-	//This enum just bitwises the levels.
-	enum levels{all=0, info=1, warning=2, error=3};
+	//!This enum just bitwises the levels.
+	enum levels{all=0, info=1, warning=2, error=4};
 
-
+	//!Converts integer to lcut values.
 	lcut int_to_lcut(int v);
+
+	//!Class constructor, creates an inactive log, with no file assigned.
 	log();
+
+	//!Class constructor, creates an active log, with a file assigned and opened.
 	log(const char * filename);
+
+	//!Class destructor. Adds a timestamp to the log file if possible (like deactivate).
 	~log();
 
+	//!Activates the log. Adds a timestamp to indicate that the session started.
 	void activate();
+
+	//!Dectivates the log. Adds a timestamp to indicate that the session ended.
 	void deactivate();
+
+	//!Inits the log, opening the file. Adds a timestamp to indicate that the session started.
 	void init(const char * filename);
 
 	//!Inserts data into the log.
-	template <class X> log& operator<<(const X &input)
-	{
+	template <class X> log& operator<<(const X &input) {
 		if(is_usable() && check_levels())
 		{
 			s<<input;
@@ -55,28 +67,56 @@ class log
 		return *this;
 	}
 
+	//!Locks or unlocks the log for multithreading environments.
+
+	//!In multithreaded environments, the log must be locked before writing
+	//!and unlocked when done.
 	log& operator<<(lop);
+
+	//!Sets the level of input, writing its tag to the output.
+
+	//!See the lin enum class for all possible input values.
 	log& operator<<(lin);
+
+	//!Sets the minimum level for the log to write data.
+
+	//!Error will show only errors, warning will show errors and warnings, etc.
+	//!See the lcut enum class for all possible input values
 	log& operator<<(lcut);
+
+	//!Adds the time, date or both to the output.
+
+	//!See the ltime enum class for all possible input values.
 	log& operator<<(ltime);
+
+	//!Accepts std::ostream modifiers.
 	log& operator<<(std::ostream& ( *pf )(std::ostream&));
+
+	//!Accepts std::ios modifiers.
 	log& operator<<(std::ios& ( *pf )(std::ios&));
+
+	//!Accepts std::ios_base modifiers.
 	log& operator<<(std::ios_base& ( *pf )(std::ios_base&));
 
 	private:
 
 	//!Checks if the current level can write.
 	bool					check_levels() {return entry_level >= min_level;}
+
 	//!Indicates if the file is open and the log is active.
 	bool 					is_usable() {return active && s.is_open();}
+
+	//!Creates a string with the system date.
 	std::string				date() const;
+
+	//!Creates a string with the system time.
 	std::string				time() const;
 
-	std::mutex				mtx;
-	std::ofstream 				s;	
-	int 					entry_level,
-						min_level;
-	bool 					active;
+	std::mutex				mtx;		//!< Internal mutex for multithreaded logging.
+	std::ofstream 				s;		//!< Internal output file stream.
+	int 					entry_level,	//!< Current log level.
+						min_level;	//!< Current minimum level to warrant logging.
+	bool 					active;		//!< Active flag.
 };
 
 }
