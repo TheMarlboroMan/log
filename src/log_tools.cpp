d4f509f172a35bebf397afd468f89b4220f59758
Daniel Pastor <dpastor@comvive.es>
2018-10-24 09:53:12 +0200

    Added log tools to the log

---
+++ src/log_tools.cpp
@@ -0,0 +1,31 @@
+#include "log_tools.h"
+
+using namespace tools;
+
+log& tools::error(tools::log& _l) {
+	_l<<tools::lop::lock<<tools::ltime::datetime<<tools::lin::error;
+	return _l;
+}
+
+log& tools::warning(tools::log& _l) {
+	_l<<tools::lop::lock<<tools::ltime::datetime<<tools::lin::warning;
+	return _l;
+}
+
+log& tools::info(tools::log& _l) {
+
+	_l<<tools::lop::lock<<tools::ltime::datetime<<tools::lin::info;
+	return _l;
+}
+
+log& tools::debug(tools::log& _l) {
+
+	_l<<tools::lop::lock<<tools::ltime::datetime<<tools::lin::debug;
+	return _l;
+}
+
+lop tools::endl(tools::log& _l) {
+
+	_l<<std::endl<<lop::unlock;
+	return lop::unlock;
+}
---
+++ src/log_tools.h
@@ -0,0 +1,17 @@
+#ifndef TOOLS_LOG_TOOLS_H
+#define TOOLS_LOG_TOOLS_H
+
+#include "log.h"
+
+namespace tools {
+//!Pairs of "lock" and "unlock" quick accesors: tools::error()<<"my stuff "<<errno<<tools::endl();
+//!These also stamp the time.
+
+log& error(tools::log& srvlog);
+log& warning(tools::log& srvlog);
+log& info(tools::log& srvlog);
+log& debug(tools::log& srvlog);
+lop endl(tools::log& srvlog);
+}
+
+#endif
