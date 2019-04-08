630c818f93ac4260bbd8009e2fe1a708b75128e1
TheMarlboroMan <marlborometal@gmail.com>
2019-04-08 21:49:26 +0200

    Added build flag for c++ revision

--- configure.sh
+++ configure.sh
@@ -2,35 +2,42 @@
 
 makefile_name=makefile;
 
-while true; do
-	echo -n "With optimizations (y/n): "
-	read with_optimizations;
-	case $with_optimizations in
-		[y]* ) optimizations="OPTIMIZATION=-O2"; break;;
-		[n]* ) optimizations="#OPTIMIZATION=-O2"; break;;
-	esac
-done;
+__retval="";
 
-while true; do
-	echo -n "With debug (y/n): "
-	read with_debug;
-	case $with_debug in
-		[y] ) debug="DEBUG=-g"; break;;
-		[n] ) debug="#DEBUG=-g"; break;;
-	esac;
-done;
+#params: prompt value_y value_n 
+#return code:0 on y and 1 on no.
+#reference return: uses __retval
+function y_n_choice() {
+
+	local read_value;
+	while true;
+	do
+		echo -n "$1 (y/n): ";
+		read read_value;
+		case $read_value in
+			"y") __retval=$2; return 0; break;;
+			"n") __retval=$3; return 1; break;;
+		esac
+	done;
+}
+
+y_n_choice "With optimizations" "OPTIMIZATION=-O2" "#OPTIMIZATION=-O2"
+optimizations=$__retval;
+
+y_n_choice "With debug" "DEBUG=-g" "#DEBUG=-g"
+debug=$__retval;
+
+y_n_choice "With C++14" "CPPREV=-std=c++14" "CPPREV=-std=c++11"
+cpprev=$__retval
 
 cp make/linux.template ./$makefile_name;
 
 sed -i -e "s/__TEMPLATE_OPTIMIZATION__/$optimizations/g" ./$makefile_name;
 sed -i -e "s/__TEMPLATE_DEBUG__/$debug/g" ./$makefile_name;
+sed -i -e "s/__TEMPLATE_CPPREV__/$cpprev/g" ./$makefile_name;
 
-while true; do
+y_n_choice "Begin compilation" "" ""
+if [ $? -eq 0 ]; then 
+	make clean; make all; echo "Done";
+fi;
 
-	echo -n "Start compilation? (y/n): "
-	read compile;
-	case $compile in
-		[y] ) make clean; make all; echo "Done"; break;;
-		[n] ) break;;
-	esac;
-done;
